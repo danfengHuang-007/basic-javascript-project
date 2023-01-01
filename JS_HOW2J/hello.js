fe4747400f08a37bf2fb8f2a9c89b1e15f1a5604
u7239623 <u7239623@anu.edu.au>
2023-01-02 08:33:05 +1100

    practice JS

--- JS_HOW2J/hello.js
+++ JS_HOW2J/hello.js
@@ -123,7 +123,7 @@ function print(message){
 print("hello")
 print("<br>");
 print("world");
-*/
+
 function print(message){
     document.write(message);
 }
@@ -133,7 +133,114 @@ function calc(x,y){
 var sum = calc(100,100);
 print(sum);
 
-
-
-
-
+var a = 0;
+function f1 (){
+    document.write('通过函数f1 设置全局变量a的值 为 5');
+    a = 5; // 这个时候 全局变量 a 的值已经改成 5了;
+}
+function f2 (){
+    document.write('通过函数f2 访问并打印全局变量a的值 '+a);
+}
+f1();
+document.write("<br>");
+f2();
+function showHello(){
+    alert('Hello Javascript');
+}
+function p(s){
+    document.write(s);
+    document.write('<br>');
+}
+p("1+2="+(1+2));
+p("5-3="+(5-3));
+p("3*2="+(3*2));
+p("9/3="+(9/3));
+p("10%3="+(10%3));
+var a = 5;
+document.write('a++ 是先取值，再运算，所以打印出来是：'+a++);
+document.write('<br>')
+var b = 5;
+document.write('++b 是先运算，再取值，所以打印出来是：'+ ++b);
+document.write('当+两边都是数字的时候，+就是加法运算符，所以1+2='+(1+2));
+document.write('<br>');
+document.write('当+任意一边是字符串的时候，+就是字符串连接符，所以1+\"2\"='+(1+'2'));
+function p(s){
+    document.write(s);
+    document.write('<br>');
+}
+p("1==2: "+(1==2));
+p("1!=2: "+(1!=2));
+p("1>2: "+(1>2));
+p("1<2: "+(1<2));
+p("1>=2: "+(1>=2));
+function p(s){
+    document.write(s);
+    document.write('<br>');
+}
+p("1=='1': "+(1=='1'));
+p("1==='1': "+(1==='1'));
+var age = 15;
+var movie = age<18? '卡通': '你懂的';
+document.write('使用?: 三相运算法进行判断。 <br>');
+document.write('age<18?"卡通":"你懂的" <br>表示当年纪小于18的时候，就看卡通，否则就看 你懂得<br>');
+document.write('而age变量的值是15，所以返回 '+movie);
+var age = 20;
+if(age<18){
+    document.write("如果年纪小于18，就看 卡通");
+}else if(age<22){
+    document.write("大于等于18，并且小于22，就看 你懂的");
+}else{
+    document.write("大于等于22就看 新闻联播");
+}
+var day = new Date().getDay();//通过日期对象获取数字形式的星期几
+var today;
+switch(day){
+    case 0:
+        today = '星期天';
+        break;
+    case 1:
+        today = '星期一';
+        break;
+    case 2:
+        today = '星期二';
+        break;
+    case 3:
+        today = '星期三';
+        break;
+    case 4:
+        today = '星期四';
+        break;
+    case 5:
+        today = '星期五';
+        break;
+    case 6:
+        today = '星期六';
+        break;
+}
+document.write("今天是 ： "+ today);
+*/
+var day = new Date().getDay();//通过日期对象获取数字形式的星期几
+var today;
+if(day==8){
+    today = '星期天';
+    
+}else if(day == 1){
+    today = '星期一';
+    
+}else if(day == 2){
+    today = '星期二';
+    
+}else if(day == 3){
+    today = '星期三';
+    
+}else if(day == 4){
+    today = '星期四';
+    
+}else if(day == 5){
+    today = '星期五';
+    document.write("今天是 ： "+ today);
+}else {
+    today = '星期六';
+    
+}
+document.write("今天是 ： "+ today);
